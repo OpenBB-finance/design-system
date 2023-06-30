a530f3d841008e34f7908e0584e7e704fa160282
José Donato <josedonato@Joses-MBP-2.lan>
2023-06-30 11:20:48 +0100

    fix: new structure (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Button from "./components/Button";
+import { Button } from "./lib";
 
 function App() {
   return (
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.js"),
+      name: "OpenBB UI",
+      fileName: (format) => `openbb-ui.${format}.js`,
+    },
+    rollupOptions: {
+      // externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
