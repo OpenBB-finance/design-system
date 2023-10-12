bf25c01076e939b076460968d31a3b727bba8e5d
Disorder <disorrder@gmail.com>
2023-10-12 15:34:09 +0200

    add button and checkbox to pro

--- packages/ui-pro/src/index.ts
+++ packages/ui-pro/src/index.ts
@@ -2,5 +2,9 @@ import "./global.css";
 
 export * from "common";
 export * from "./components";
+
+export * from "../../ui/src/components/ui/Button";
+export * from "../../ui/src/components/ui/Checkbox";
+
 export const VERSION = import.meta.env.VITE_VERSION;
 export const BUILD_INFO = import.meta.env.VITE_BUILD_INFO;
