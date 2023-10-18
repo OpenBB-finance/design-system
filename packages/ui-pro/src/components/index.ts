6623344cf8448df29a99cf3a9555e71bdcf316ef
Disorder <disorrder@gmail.com>
2023-10-18 17:36:47 +0200

    fix: add Tabs to build

--- packages/ui-pro/src/components/index.ts
+++ packages/ui-pro/src/components/index.ts
@@ -4,3 +4,5 @@ export * from "@openbb/ui/src/components/ui/Button";
 export * from "@openbb/ui/src/components/ui/Checkbox";
 export * from "@openbb/ui/src/components/ui/Form";
 export * from "@openbb/ui/src/components/ui/RadioGroup";
+
+export * from "./ui/Tabs";
