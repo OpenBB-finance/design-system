66caa6e151e1cf23d9a2cc8e360a5622adaaa19d
Disorder <disorrder@gmail.com>
2024-01-15 16:47:21 +0100

    fix typography titles and subtitles

--- packages/common/plugins/typography.ts
+++ packages/common/plugins/typography.ts
@@ -4,31 +4,31 @@ const STYLE = {
   title: {
     fontFamily: "aktiv-grotesk-extended, Inter",
     textTransform: "uppercase",
+    letterSpacing: "0.1em",
   },
-  subtitle: { fontFamily: "Inter", textTransform: "uppercase" },
+  subtitle: {
+    fontFamily: "Inter",
+    textTransform: "uppercase",
+    letterSpacing: "0.1em",
+  },
   body: { fontFamily: "Inter" },
 } as const;
 
 const SIZE = {
   title: {
-    xs: { fontSize: "1.5rem", lineHeight: "2.25rem", letterSpacing: "0.15rem" },
-    sm: {
-      fontSize: "1.75rem",
-      lineHeight: "2.625rem",
-      letterSpacing: "0.175rem",
-    },
-    md: { fontSize: "2rem", lineHeight: "3rem", letterSpacing: "0.2rem" },
-    lg: { fontSize: "2.5rem", lineHeight: "3.75rem", letterSpacing: "0.25rem" },
-    xl: { fontSize: "3rem", lineHeight: "4rem", letterSpacing: "0.3rem" },
+    xs: { fontSize: "1.5rem", lineHeight: "2.25rem" },
+    sm: { fontSize: "1.75rem", lineHeight: "2.625rem" },
+    md: { fontSize: "2rem", lineHeight: "3rem" },
+    lg: { fontSize: "2.5rem", lineHeight: "3.75rem" },
+    xl: { fontSize: "3rem", lineHeight: "4rem" },
   },
   subtitle: {
-    md: {
-      fontSize: "1.125rem",
-      lineHeight: "1.75rem",
-      letterSpacing: "0.1125rem",
-    },
-    lg: { fontSize: "1.25rem", lineHeight: "2rem", letterSpacing: "0.125rem" },
-    xl: { fontSize: "1.5rem", lineHeight: "2rem", letterSpacing: "0.15rem" },
+    "2xs": { fontSize: "0.625rem", lineHeight: "1rem" },
+    xs: { fontSize: "0.75rem", lineHeight: "1.125rem" },
+    sm: { fontSize: "0.875rem", lineHeight: "1.375rem" },
+    md: { fontSize: "1.125rem", lineHeight: "1.75rem" },
+    lg: { fontSize: "1.25rem", lineHeight: "2rem" },
+    xl: { fontSize: "1.5rem", lineHeight: "2rem" },
   },
   body: {
     xs: { fontSize: "0.75rem", lineHeight: "1.125rem" },
