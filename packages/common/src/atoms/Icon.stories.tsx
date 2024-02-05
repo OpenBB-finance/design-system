fcce6d8d2e78081fd98871846b428ec98667fb5e
Disorder <disorrder@gmail.com>
2024-02-05 18:36:25 +0100

    fix icons stories

--- packages/common/src/atoms/Icon.stories.tsx
+++ packages/common/src/atoms/Icon.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { iconNames } from "~/icons/icons";
+import { iconNames } from "../icons/icons";
 import { Icon } from "./Icon";
 
 const meta = {
