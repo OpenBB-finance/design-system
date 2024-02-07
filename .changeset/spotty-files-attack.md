a9de9de4d584df6a8f60aa8c36894b04431d191c
Disorder <disorrder@gmail.com>
2024-02-07 17:20:56 +0100

    fix imports

---
+++ .changeset/spotty-files-attack.md
@@ -0,0 +1,6 @@
+---
+"@openbb/ui-pro": patch
+"@openbb/ui": patch
+---
+
+fix imports that doesn't work on 0.5.8 💀
--- packages/common/src/atoms/index.ts
+++ packages/common/src/atoms/index.ts
@@ -1,4 +1,5 @@
 export * from "./Alert";
+export * from "./Avatar";
 export * from "./Button";
 export * from "./Checkbox";
 export * from "./CopyButton";
