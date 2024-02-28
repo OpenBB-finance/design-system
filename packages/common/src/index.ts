34d9f5c3f08072900cf5cda2f4ed753422290949
Disorder <disorrder@gmail.com>
2024-02-28 18:19:58 +0100

    feat: add Carousel to exports 🤡

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./atoms";
 export * from "./dialogs";
+export * from "./molecules";
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./atoms";
 // Molecules
 export * from "./dialogs";
+export * from "./molecules";
