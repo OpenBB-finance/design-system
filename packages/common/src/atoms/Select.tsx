a963c4914c9ac372d3cc0d8ae8fa95849d71f799
Disorder <disorrder@gmail.com>
2024-03-05 20:38:26 +0100

    fix lint

--- packages/common/src/atoms/Select.tsx
+++ packages/common/src/atoms/Select.tsx
@@ -153,7 +153,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "space-y-2 p-2",
           position === "popper" &&
-            "h-radix-select-trigger-height w-full min-w-[var(--radix-select-trigger-width)]",
+            "w-full min-w-[var(--radix-select-trigger-width)] h-radix-select-trigger-height",
         )}
       >
         {children}
